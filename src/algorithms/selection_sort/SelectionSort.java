457bc723e65b0814fe5a077255254a9a1d6d5558
Ryan Shin <changyeolshin@gmail.com>
2018-08-28 22:39:11 -0700

    Created the Selection Sort algorithm.

--- src/algorithms/selection_sort/SelectionSort.java
+++ src/algorithms/selection_sort/SelectionSort.java
@@ -1,17 +1,20 @@
-package algorithms.bubble_sort;
+package algorithms.selection_sort;
 
-public class BubbleSort {
+public class SelectionSort {
 
     // O(n^2)
     public static void main(String[] args) {
         int[] intArray = { 99, 23, 2, 14, -92, -3, 0, -7, 87, 64 };
 
         for (int i = intArray.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (intArray[j] > intArray[i]) {
-                    swap(j, i, intArray);
+            int indexWithLargest = 0;
+
+            for (int j = 1; j < i; j++) {
+                if (intArray[j] > intArray[indexWithLargest]) {
+                    indexWithLargest = j;
                 }
             }
+            swap(indexWithLargest, i, intArray);
         }
 
         for (int number: intArray) {
